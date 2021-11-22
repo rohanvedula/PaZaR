132e001eedd72c93b60cff31bb5c3c4f461463d9
programmer812 <64325806+programmer812@users.noreply.github.com>
2021-11-22 10:18:54 -0500

    Updated Append_Display Function

--- text_to_latex/text_to_latex.py
+++ text_to_latex/text_to_latex.py
@@ -69,4 +69,35 @@ def append_text(doc: Document, text: str) -> Document:
 # function to append display math: INCOMPLETE
 def append_display(doc: Document, text: str) -> Document:
     
+    strings = []
+
+    prev_ind = 0
+    
+    in_math = False
+    
+    for i, char in enumerate(text):
+        
+        if (char == '$' and text[i + 1] == '$') or (char == '[' or char == ']'):
+            # if not already in math mode, append text to string array and start search for end of math
+            if not in_math:
+                strings.append(text[prev_ind:i])
+                in_math = True
+                prev_ind = i
+            else:   # if already in math mode, append equation to string array and get out of math search
+                math_string = NoEscape(r"{}".format(text[prev_ind+1:i]))
+                strings.append(Math(data=[math_string], inline=True))
+                in_math = False
+                prev_ind = i+1
+
+    # append final string
+    strings.append(text[prev_ind:])
+
+    # append all strings into one line
+    for string in strings:
+        doc.append(string)
+
+    # generate pdf and tex
+    doc.generate_pdf()
+    doc.generate_tex()
+    
     return doc
